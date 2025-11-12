23ccf00534b03828ce4d830e7e681e464b68cf24
Ximin Piao <pxm726@gmail.com>
2025-11-12 08:50:20 -0600

    changed miscanthus singlesite_calibration to use UTR C allocation model and the newest biocro-dev

---
+++ calibrations/miscanthus/code/miscanthus_utr_params.R
@@ -0,0 +1,187 @@
+# The variables commented with "optimize" are UTR specific variables
+parameters <- list(
+  # Parameters unrelated to any module
+  timestep = 1.0,
+  
+  default_carbon_to_mass_factor <- 0.3, # Mg/ha / (mol/m^2)
+  base_utilization_rate_constant <- 1e-2, # optimize
+  base_utilization_km <- 0.1, # optimize
+  base_conductance <- 0.1, # optimize
+  # Parameters related to the UTR model
+  Leaf_carbon_to_mass_factor = default_carbon_to_mass_factor, 
+  Leaf_utilization_rate_constant = 1.0 * base_utilization_rate_constant, # optimize
+  Leaf_utilization_km = 1.0 * base_utilization_km, # optimize
+  Leaf_respiration_factor = 0.0,				# Leaf respiration is accounted for
+  # by the canopy photosynthesis module
+  Stem_carbon_to_mass_factor = default_carbon_to_mass_factor, 
+  Stem_utilization_rate_constant = 1.5 * base_utilization_rate_constant, # optimize
+  Stem_utilization_km = 1.0 * base_utilization_km, # optimize
+  Stem_respiration_factor = 0.02, # optimize
+  
+  Root_carbon_to_mass_factor = default_carbon_to_mass_factor, # optimize
+  Root_utilization_rate_constant = 0.2 * base_utilization_rate_constant, # optimize
+  Root_utilization_km = 0.5 * base_utilization_km, # optimize
+  Root_respiration_factor = 0.03,
+  
+  Rhizome_carbon_to_mass_factor = default_carbon_to_mass_factor,
+  Rhizome_utilization_rate_constant = 0.1 * base_utilization_rate_constant, # optimize
+  Rhizome_utilization_km = 2.0 * base_utilization_km, # optimize
+  Rhizome_respiration_factor = 0.02,
+  
+  substrate_conductance_Leaf_to_Stem = base_conductance,
+  substrate_conductance_Stem_to_Root = 0.4 * base_conductance, # optimize
+  substrate_conductance_Stem_to_Rhizome = 5*base_conductance, # optimize
+   
+  early_growth_end_dvi =                   0.3, # optimize
+  substrate_use_refill_ratio =             0.5, # optimize
+  
+  transportation_beta_exponent = 1,
+  
+  # senescence_coefficient_logistic module
+  Leaf_senescence_fraction_max =           0.0005, # optimize
+  Stem_senescence_fraction_max =           0.0005, # optimize
+  Root_senescence_fraction_max =           0.0005,  # optimize
+  Rhizome_senescence_fraction_max  =       0.000, # optimize
+  Leaf_senescence_alpha    =               1.0,   # optimize
+  Stem_senescence_alpha    =               1.0,  # optimize
+  Root_senescence_alpha    =               0.5,    # optimize
+  Rhizome_senescence_alpha     =           0.1, # optimize
+  Leaf_senescence_beta     =               1.5, # optimize
+  Stem_senescence_beta     =               1.5, # optimize
+  Root_senescence_beta     =               2.0, # optimize
+  Rhizome_senescence_beta      =           1, # optimize
+  Leaf_senescence_reuse_factor =           0.5, # optimize
+  Stem_senescence_reuse_factor =           0.5, # optimize
+  Root_senescence_reuse_factor =           0.5, # optimize
+  Rhizome_senescence_reuse_factor =        0, # optimize
+  
+  
+  # Parameters related to the `parameter_calculator` module
+  iSp = 1.7, # updated 
+  Sp_thermal_time_decay = 0.00038, # updated
+  LeafN_0 = 2.0,
+  LeafN = 2.0,
+  vmax_n_intercept = 0,
+  Vcmax_at_25 = 39, # updated # Renamed from "vmax1"
+  alphab1 = 0,
+  alpha1 = 0.04, # 0 in biomass-crops-marginal-land, 0.04 in miscanthus_x_giganteous.R, 0.86 in Collatz et al. 1992 paper.
+  
+  # carbon_assimilation_to_biomass
+  dry_biomass_per_carbon = 30.026, # # g CHO / mol C (glucose)
+  
+  # soil parameters (clay loam)
+  soil_air_entry              = -2.6,
+  soil_b_coefficient          = 5.2,
+  soil_bulk_density           = 1.35,
+  soil_clay_content           = 0.34,
+  soil_field_capacity         = 0.32,
+  soil_sand_content           = 0.32,
+  soil_saturated_conductivity = 6.4e-05,
+  soil_saturation_capacity    = 0.52,
+  soil_silt_content           = 0.34,
+  soil_wilting_point          = 0.2,
+  
+  # Parameters related to the `soil_evaporation` module
+  rsec = 0.2,
+  soil_clod_size = 0.04,
+  soil_reflectance = 0.2,
+  soil_transmission = 0.01,
+  specific_heat_of_air=1010,
+  #  specific_heat = 1010,
+  stefan_boltzman = 5.67e-8,
+  
+  # thermal_time_development_rate_calculator module
+  sowing_fractional_doy =                  0      , # from miscanthus_x_giganteous.R, non-existing in yufeng's biomass-crops-marginal-land
+  TTemr =                                  400    ,  # Emergence, Tejera & Heaton 2019 ; 400 from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  TTveg =                                  1600   ,  # GGD6 = 2200, Assume it is stem elongation. Tejera & Heaton 2019; 900 from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  TTrep =                                  400   , # GGD6 = 2600, Leaf growth stop, Tejera & Heaton 2019 # from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  
+  # thermal_time_trilinear
+  topt_lower =                            28, # from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  topt_upper =                            31, # from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  tmax =                                  40, # from yufeng's biomass-crops-marginal-land, non existing in miscanthus_x_giganteous.R
+  
+  # incident_shortwave_from_ground_par module
+  par_energy_fraction            =        0.5,
+  par_energy_content             =        0.235,
+  
+  # height_from_lai module
+  heightf                     = 3,           # Yufeng
+  
+  # canopy_gbw_thornley module
+  min_gbw_canopy              = 0.005,       # m / s
+  
+  # stefan_boltzmann_longwave module
+  emissivity_sky              = 1,
+  
+  # two_layer_soil_profile module
+  soil_depth1=                             0.0,         # meters
+  soil_depth2=                             2.5,         # meters
+  soil_depth3=                             10.0,        # meters
+  wsFun     =                              2,           # not used, but must be defined
+  hydrDist  =                              0 ,          # same as in sorghum parameter file
+  rfl       =                              0.2,         # same as in sorghum parameter file
+  rsdf      =                              0.44,        # same as in sorghum parameter file
+  phi1      =                              0.01,
+  phi2      =                              1.5,          # from Sugarcane-BioCro, Jaiswal et al. 2017 (https://doi.org/10.1038/nclimate3410)
+  
+  # thermal_time_linear module
+  tbase     =                              6,          # degrees C, Tejera & Heaton 2017.(Originally 10)
+  
+  # solar_position_michalsky module
+  lat                         =            40,
+  longitude                   =            -88,
+  time_zone_offset            =            -6,
+  
+  # shortwave_atmospheric_scattering module
+  atmospheric_pressure  =                  101325,
+  atmospheric_transmittance =              0.85,       # Campbell and Norman, An Introduction to Environmental Biophysics, 2nd Edition, Pg 173
+  atmospheric_scattering  =                0.3, 
+  
+  # ten_layer_canopy_properties module
+  chil                        = 1,           # 1 in miscanthus_x_giganteous.R,  0.81, Campbell and Norman, An Introduction to Environmental Biophysics, 2nd Edition, Table 15.1, pg 253
+  k_diffuse                   = 0.7,         # Estimated from Campbell and Norman, An Introduction to Environmental Biophysics, 2nd Edition, Figure 15.4, pg 254 # 0.1
+  kpLN                        = 0,           # not used in Soybean-BioCro
+  leaf_reflectance_nir        = 0.38,        # Corn values from Table 7-1 from Norman & Arkebauer (1991) https://doi.org/10.2134/agronmonogr31.c7
+  leaf_reflectance_par        = 0.09,        # Corn values from Table 7-1 from Norman & Arkebauer (1991) https://doi.org/10.2134/agronmonogr31.c7
+  leaf_transmittance_nir      = 0.45,        # Corn values from Table 7-1 from Norman & Arkebauer (1991) https://doi.org/10.2134/agronmonogr31.c7
+  leaf_transmittance_par      = 0.04,        # Corn values from Table 7-1 from Norman & Arkebauer (1991) https://doi.org/10.2134/agronmonogr31.c7
+  lnfun                       = 0,           # not used in Soybean-BioCro
+  
+  # ten_layer_c4_canopy module
+  # jmax                        = 195,         # Bernacchi et al. 2005 (https://doi.org/10.1007/s00425-004-1320-8), 2002 Seasonal average
+  # jmax_mature                 = 195,         # Needed in the varying_Jmax25 module
+  sf_jmax                     = 0.2,         # Scaling factor for jmax. Needed in the varying_Jmax25 module
+  electrons_per_carboxylation = 4.5,         # Bernacchi et al. 2003 (https://doi.org/10.1046/j.0016-8025.2003.01050.x)
+  electrons_per_oxygenation   = 5.25,        # Bernacchi et al. 2003 (https://doi.org/10.1046/j.0016-8025.2003.01050.x)
+  tpu_rate_max                = 13,          # Fitted value based on the A-Ci data measured at UIUC in 2019-08 by Delgrado (unpublished data)
+  RL_at_25                    = 1.28,        # Davey et al. 2004 (https://doi.org/10.1104/pp.103.030569), Table 3, cv Pana, co2 368 ppm # Renamed from `Rd`
+  Catm                        = 372.59,      # micromol / mol, CO2 level in 2002
+  O2                          = 210,         # millimol / mol
+  b0                          = 0.08,        # miscanthus_x_giganteus.R
+  b1                          = 3,           # miscanthus_x_giganteus.R
+  Gs_min                      = 1e-3,
+  theta                       = 0.83,        # miscanthus_x_giganteous.R
+  windspeed_height            = 10,
+  leafwidth                   = 0.1,         # Large mature leaflets can reach 10 cm in width
+  beta                        = 0.93,        # from miscanthus_x_giganteus.R in data file, no reference
+  kparm                       = 0.7,         # from miscanthus_x_giganteus.R in data file, no reference
+  lowerT                      = 3,           # from miscanthus_x_giganteus.R in data file, no reference, 10 in yufeng's biomass-crops-marginal-land setting
+  upperT                      = 37.5,        # from miscanthus_x_giganteus.R in data file, no reference
+  # ten_layer_canopy_integrator module
+  
+  growth_respiration_fraction = 0,
+  # c4_canopy, all from miscanthus_x_giganteous.R
+  nalphab0 = 0.02367,
+  nalphab1 = 0.000488,
+  nileafn = 85,
+  nkln = 0.5,
+  nkpLN = 0.17,
+  nlayers = 10,
+  nlnb0 = -5,
+  nlnb1 = 18,
+  nRdb0 = -4.5917,
+  nRdb1 = 0.1247,
+  nvmaxb0 = -16.25,
+  nvmaxb1 = 0.6938
+)
---
+++ calibrations/miscanthus/code/singlesite_calibration_utr_biocro-dev.R
@@ -0,0 +1,142 @@
+library(BioCro)
+library(UTRMiscanthusBML)
+# Adapted from Yufeng He: Dec 12, 2022 by Ximin on Sep 24, 2025
+
+# parameters and weather inputs
+
+load("../data//parameters/miscanthus_giganteus_initial_state.rdata")
+load("../data//parameters/miscanthus_giganteus_logistic_parameters.rdata")
+load("../data//parameters/miscanthus_giganteus_ss_logistic_modules.rdata")
+load("../data//parameters/miscanthus_giganteus_deriv_logistic_modules.rdata")
+source('miscanthus_utr_params.R')
+source('miscanthus_utr_initial_values.R')
+
+miscanthus_giganteus_direct_utr_modules <- c("BioCro:stomata_water_stress_linear",
+                                             "BioCro:leaf_water_stress_exponential",
+                                             "UTRMiscanthusBML:lai_from_structural_carbon",
+                                             #"BioCro:parameter_calculator",
+                                             "BioCro:solar_position_michalsky",
+                                             # "BioCro:shortwave_atmospheric_scattering", # replaced by c4_canopy
+                                             # "BioCro:incident_shortwave_from_ground_par", # replaced by c4_canopy
+                                             # "BioCro:ten_layer_canopy_properties", # replaced by c4_canopy
+                                             # "BioCro:ten_layer_c4_canopy", # replaced by c4_canopy
+                                             canopy_photosynthesis = "BioCro:c4_canopy",
+                                             "BioCro:stefan_boltzmann_longwave",
+                                             "BioCro:canopy_gbw_thornley",
+                                             "BioCro:height_from_lai",
+                                             # "BioCro:ten_layer_canopy_integrator", # replaced by c4_canopy
+                                             "BioCro:carbon_assimilation_to_biomass",
+                                             # "BioCro:thermal_time_development_rate_calculator",
+                                             "BioCro:development_index_from_thermal_time",
+                                             "UTRMiscanthusBML:thornley_utilization_calculator_lsrr",
+                                             "UTRMiscanthusBML:thornley_transport_calculator_lsrr",
+                                             "UTRMiscanthusBML:thornley_biomass_calculator_lsrr")
+
+miscanthus_giganteus_differential_utr_modules <- c("UTRMiscanthusBML:thornley_utilization_lsrr",
+                                                   "UTRMiscanthusBML:thornley_transport_lsrr",
+                                                   "BioCro:two_layer_soil_profile",
+                                                   # "BioCro:development_index",
+                                                   "BioCro:thermal_time_trilinear")
+
+miscanthus_giganteus_utr_parameters <- parameters
+miscanthus_giganteus_initial_state <- initial_state
+
+
+weather_all<-read.csv(
+  "../data/weather_data_NASA_POWER/BioCro_input_NASA/site_2_2002_2018_LowerTransmittance.csv")
+result = list()
+years = 2006:2008
+for (i in 1:length(years)){
+  year_i = years[i]
+  growing_season_weather = weather_all[weather_all$year == year_i,]
+  growing_season_weather =  growing_season_weather[growing_season_weather$doy>=106 &
+                                                    growing_season_weather$doy<=350,]
+  
+  
+  
+  result[[i]] <- run_biocro(initial_values =  miscanthus_giganteus_initial_state,
+                       parameters = miscanthus_giganteus_utr_parameters,
+                       drivers = growing_season_weather,
+                       direct_module_names = miscanthus_giganteus_direct_utr_modules,
+                       differential_module_names = miscanthus_giganteus_differential_utr_modules, 
+                       ode_solver = BioCro::default_ode_solvers$boost_rkck54,verbose = FALSE)
+  
+  
+  ##########################################################################################
+  # Correcting for winter loss of atem based on 0.07 tons/ha per day
+  # non_frost_weather <- get_growing_season_climate(growing_season_weather, threshold_temperature = 0)
+  # 
+  # for ( j in dim(non_frost_weather)[1]: (dim(growing_season_weather)[1])){
+  #   result[[i]]$Stem[j] = result[[i]]$Stem[j] - (0.07/24)*(j- dim(non_frost_weather)[1])
+  # }
+  ########################################################################################
+}
+
+library(lattice)
+xyplot(data=result[[1]], canopy_assimilation_rate~time)
+xyplot(data=result[[1]], DVI~doy)
+xyplot(data=result[[1]], DVI~TTc)
+xyplot(data=result[[1]], TTc~time)
+# xyplot(data=result[[1]], Leaf_senescence_loss~time)
+
+# plot the substrate/storage C "concentrations"
+concentrations <- with(result[[1]], data.frame(
+  doy = doy,
+  Leaf = Leaf_substrate_carbon/Leaf_structural_carbon,
+  Stem = Stem_substrate_carbon/Stem_structural_carbon,
+  Root = Root_substrate_carbon/Root_structural_carbon,
+  Rhizome = Rhizome_substrate_carbon/Rhizome_structural_carbon,
+  Rhizome_storage = (Rhizome_substrate_carbon+Rhizome_storage_carbon)/Rhizome_structural_carbon
+))
+
+xyplot(data=concentrations, 
+       Leaf + Stem + Root + Rhizome + Rhizome_storage ~ doy, 
+       ylab = "Substrate C: Structural C",
+       auto.key = TRUE)
+
+# plot the utilization rate compared to transport to check 
+# 1. if there is significant imbalances
+# 2. if the rhizome exports first and then import C
+# 3. if Stem_utilization_rate is high enough for the rapid growth
+# 4. if not is it limited by the stem substrate concentration or the utilization parameters?
+xyplot(data=result[[1]], 
+       Leaf_utilization_rate+
+         substrate_transport_Leaf_to_Stem+
+         Stem_utilization_rate+
+         substrate_transport_Stem_to_Rhizome
+       ~doy, auto.key = TRUE)
+
+# plot the utilization rate compared to transport to check if there is significant imbalances
+xyplot(data=result[[1]], 
+         substrate_transport_Leaf_to_Stem+
+         substrate_transport_Stem_to_Root+
+         substrate_transport_Stem_to_Rhizome
+       ~doy, auto.key = TRUE)
+
+
+source("plot_single_site.R")
+#avg of 2006-2008
+predicted = 0
+for (i in 1:length(years)){
+  predicted = predicted+result[[i]][,c("doy","Stem","Leaf","Rhizome","Root")]
+}
+predicted = predicted/length(years)
+predicted = reshape2::melt(predicted,id.vars = c("doy"),measure.vars = c("Leaf","Stem","Root","Rhizome"))
+
+observed_biomass <- read.csv("../data/biomass_observation/il_observation_without_index.csv")
+
+names(observed_biomass)  = c("doy", "Rhizome",     "Leaf"     ,"Stem", "Root")
+observed_biomass$Root[1] = 0 # First data point of root should be zero. Observation does not make difference between alive and dead roots
+#observed <- observed_biomass[1:4, c("doy","Stem","Leaf","Rhizome","Root","lai")]
+observed = observed_biomass[, c("doy","Stem","Leaf","Rhizome","Root")]
+observed = reshape2::melt(observed,id.vars = c("doy"),measure.vars = c("Leaf","Stem","Root","Rhizome"))
+names(observed)  = c("x","varname","y")
+names(predicted) = c("x","varname","y")
+xlabtitle = expression(paste("Day of Year"))
+ylabtitle = expression(paste("Dry Biomass (Mg/ha)"))
+BiomassPartitioning <- 
+  Compare_Observed_and_Predicted(observeddata = observed,predicteddata=predicted  ,xlabtitle,ylabtitle)
+plot(BiomassPartitioning)
+# ggsave(filename="./BiomassPartitioning.png",dpi = 500, width =6.5, height =4.2)
+
+doy = result[[1]]$doy
